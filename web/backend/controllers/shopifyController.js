11036456cb7574efaa90288b62e0df42652f2815
Emmy <emmyconceptng@gmail.com>
2025-04-12 09:17:56 +0100

    webhook handler

--- web/backend/controllers/shopifyController.js
+++ web/backend/controllers/shopifyController.js
@@ -221,7 +221,6 @@ export async function fetchShopifyStoreDetails(session) {
                       createdAt
                       publishedAt
                       tags
-                      content
                       contentHtml
                       image {
                         url
