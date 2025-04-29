73a0d480060aabd6dd5f63e155262a55ad179901
Emmy <emmyconceptng@gmail.com>
2025-04-29 03:19:14 +0100

    exit frame error

--- web/frontend/pages/ExitIframe.jsx
+++ web/frontend/pages/ExitIframe.jsx
@@ -1,43 +1,62 @@
-import { useAppBridge } from "@shopify/app-bridge-react";
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { Banner, Layout, Page } from "@shopify/polaris";
+import { BrowserRouter } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { NavMenu } from "@shopify/app-bridge-react";
+import Routes from "./Routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { QueryProvider, PolarisProvider } from "./components";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./store";
+import { Provider } from "react-redux";
+import { useEffect } from "react"; // import useEffect
 
-export default function ExitIframe() {
-  const app = useAppBridge();
-  const { search } = useLocation();
-  const [showWarning, setShowWarning] = useState(false);
-
-  app.loading(true);
+export default function App() {
+  const pages = import.meta.glob("./pages/**/!(*.test.[jt]sx)*.([jt]sx)", {
+    eager: true,
+  });
+  const { t } = useTranslation();
 
   useEffect(() => {
-    if (!!app && !!search) {
-      const params = new URLSearchParams(search);
-      const redirectUri = params.get("redirectUri");
-      const url = new URL(decodeURIComponent(redirectUri));
+    // Detect if inside iframe
+    if (window.top !== window.self) {
+      const params = new URLSearchParams(window.location.search);
+      const shop = params.get("shop");
+      const host = params.get("host");
 
-      if (
-        [location.hostname, "admin.shopify.com"].includes(url.hostname) ||
-        url.hostname.endsWith(".myshopify.com")
-      ) {
-        window.open(url, "_top");
-      } else {
-        setShowWarning(true);
+      if (shop && host) {
+        const redirectUri = encodeURIComponent(
+          `https://${window.location.hostname}/?shop=${shop}&host=${host}`
+        );
+        window.location.assign(`/exitiframe?redirectUri=${redirectUri}`);
       }
     }
-  }, [app, search, setShowWarning]);
+  }, []);
 
-  return showWarning ? (
-    <Page narrowWidth>
-      <Layout>
-        <Layout.Section>
-          <div style={{ marginTop: "100px" }}>
-            <Banner title="Redirecting outside of Shopify" status="warning">
-              Apps can only use /exitiframe to reach Shopify or the app itself.
-            </Banner>
-          </div>
-        </Layout.Section>
-      </Layout>
-    </Page>
-  ) : null;
+  return (
+    <PolarisProvider>
+      <BrowserRouter>
+        <QueryProvider>
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={true}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+              />
+              <NavMenu>
+                <a href="/" rel="home" />
+              </NavMenu>
+              <Routes pages={pages} />
+            </PersistGate>
+          </Provider>
+        </QueryProvider>
+      </BrowserRouter>
+    </PolarisProvider>
+  );
 }
