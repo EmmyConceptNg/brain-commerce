8d27f35cdbab0dc25217e5bcf12a2a0d5545ac75
Emmy <emmyconceptng@gmail.com>
2025-04-29 03:23:09 +0100

    exit frame error

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -4,43 +4,57 @@ import { NavMenu } from "@shopify/app-bridge-react";
 import Routes from "./Routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import { QueryProvider, PolarisProvider } from "./components";
-import { PersistGate } from 'redux-persist/integration/react'; // Import PersistGate
-import { persistor, store } from './store'; // Import persistor and store
-import { Provider } from 'react-redux'; // Import Provider from react-redux
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./store";
+import { Provider } from "react-redux";
+import { useEffect } from "react"; // import useEffect
 
 export default function App() {
-  // Any .tsx or .jsx files in /pages will become a route
-  // See documentation for <Routes /> for more info
   const pages = import.meta.glob("./pages/**/!(*.test.[jt]sx)*.([jt]sx)", {
     eager: true,
   });
   const { t } = useTranslation();
 
+  useEffect(() => {
+    // Detect if inside iframe
+    if (window.top !== window.self) {
+      const params = new URLSearchParams(window.location.search);
+      const shop = params.get("shop");
+      const host = params.get("host");
+
+      if (shop && host) {
+        const redirectUri = encodeURIComponent(
+          `https://${window.location.hostname}/?shop=${shop}&host=${host}`
+        );
+        window.location.assign(`/exitiframe?redirectUri=${redirectUri}`);
+      }
+    }
+  }, []);
+
   return (
     <PolarisProvider>
       <BrowserRouter>
         <QueryProvider>
-          <Provider store={store}> {/* Wrap with Provider */}
-            <PersistGate loading={null} persistor={persistor}> {/* Add PersistGate */}
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
               <ToastContainer
-                    position="top-right"
-                    autoClose={5000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={true}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                  />
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={true}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+              />
               <NavMenu>
                 <a href="/" rel="home" />
               </NavMenu>
               <Routes pages={pages} />
-            </PersistGate> {/* Close PersistGate */}
-          </Provider> {/* Close Provider */}
+            </PersistGate>
+          </Provider>
         </QueryProvider>
       </BrowserRouter>
     </PolarisProvider>
